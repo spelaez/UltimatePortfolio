e6485595b1d9f245cc9a46feac9e70087317f855
Santiago Pelaez <santirua@gmail.com>
2021-04-29 10:18:19 -0500

    initial commit

---
+++ UltimatePortfolio/UltimatePortfolio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UltimatePortfolio
+//
+//  Created by Santiago Pelaez Rua on 29/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UltimatePortfolio/UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Santiago Pelaez Rua on 29/04/21.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
