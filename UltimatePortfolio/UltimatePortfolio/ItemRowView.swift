20382a55565faef54b99a7734cfdb78998cd0d47
Santiago Pelaez <santirua@gmail.com>
2021-05-21 14:45:00 -0500

    finishing projects view

--- UltimatePortfolio/UltimatePortfolio/ItemRowView.swift
+++ UltimatePortfolio/UltimatePortfolio/ItemRowView.swift
@@ -8,17 +8,37 @@
 import SwiftUI
 
 struct ItemRowView: View {
+    @ObservedObject var project: Project
     @ObservedObject var item: Item
+    
+    var icon: some View {
+        let projectColor = Color(project.projectColor)
+        
+        if item.completed {
+            return Image(systemName: "checkmark.circle")
+                .foregroundColor(projectColor)
+        } else if item.priority == 3 {
+            return Image(systemName: "exclamationmark.triangle")
+                .foregroundColor(projectColor)
+        } else {
+            return Image(systemName: "checkmark.circle")
+                .foregroundColor(.clear)
+        }
+    }
     
     var body: some View {
         NavigationLink(destination: EditItemView(item: item)) {
-            Text(item.itemTitle)
+            Label {
+                Text(item.itemTitle)
+            } icon: {
+                icon
+            }
         }
     }
 }
 
 struct ItemRowView_Previews: PreviewProvider {
     static var previews: some View {
-        ItemRowView(item: .example)
+        ItemRowView(project: .example, item: .example)
     }
 }
