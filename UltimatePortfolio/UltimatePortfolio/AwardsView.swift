b5a470f63c6e95f03b9cfa43943882dab086d075
Santiago Pelaez <santirua@gmail.com>
2021-09-20 22:37:52 -0500

    Counting Core Data results

--- UltimatePortfolio/UltimatePortfolio/AwardsView.swift
+++ UltimatePortfolio/UltimatePortfolio/AwardsView.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 struct AwardsView: View {
     static let tag: String? = "Awards"
+
+    @EnvironmentObject var dataController: DataController
+    @State private var selectedAward = Award.example
+    @State private var showingAwardDetails = false
+    @State private var alertTitle: String = "Locked"
     
     var columns: [GridItem] {
         [GridItem(.adaptive(minimum: 100, maximum: 100))]
@@ -20,25 +25,32 @@ struct AwardsView: View {
                 LazyVGrid(columns: columns) {
                     ForEach(Award.allAwards) { award in
                         Button {
-                            // no action yet
+                            selectedAward = award
+
+                            alertTitle = dataController.hasEarned(award: award) ? "Unlocked: \(selectedAward.name)" : "Locked"
+                            showingAwardDetails = true
                         } label: {
                             Image(systemName: award.image)
                                 .resizable()
                                 .scaledToFit()
                                 .padding()
                                 .frame(width: 100, height: 100)
-                                .foregroundColor(.secondary.opacity(0.5))
+                                .foregroundColor(dataController.hasEarned(award: award) ? Color(award.color) : .secondary.opacity(0.5))
                         }
                     }
                 }
             }
             .navigationTitle("Awards")
         }
+        .alert(alertTitle, isPresented: $showingAwardDetails, actions: {}, message: {
+            Text(selectedAward.description)
+        })
     }
 }
 
 struct AwardsView_Previews: PreviewProvider {
     static var previews: some View {
         AwardsView()
+            .environmentObject(DataController.preview)
     }
 }
