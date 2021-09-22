ce0c11f42d6d5096098d360c26fb39dab66edf6a
Santiago Pelaez <santirua@gmail.com>
2021-09-21 19:20:38 -0500

    Creating the Home view

---
+++ UltimatePortfolio/UltimatePortfolio/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  UltimatePortfolio
+//
+//  Created by Santiago Pelaez Rua on 21/09/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(uiColor: .systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(uiColor: .secondarySystemGroupedBackground)
+}
